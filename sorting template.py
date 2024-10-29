51bc34ed4bcdf70c34ac1446a6d1bda6445231fb
Nitterbomb <154538827+Nitterbomb@users.noreply.github.com>
2024-10-28 20:49:35 -0400

    Add files via upload

--- sorting template.py
+++ sorting template.py
@@ -1,5 +1,6 @@
 # Sorting template
 import random
+import time
 
 def bubble_sort(unsorted_list):
     sorted_list = unsorted_list[:]
@@ -29,15 +30,34 @@ def quick_sort(unsorted_list):
 def main():
     to_be_sorted = [random.randint(1,100) for i in range(100)]
     print("Bubble Sort")
+    start_time = time.time()
     print(bubble_sort(to_be_sorted))
-    print("Selection Sort")
-    print(selection_sort(to_be_sorted))
-    print("Insertion Sort")
-    print(insertion_sort(to_be_sorted))
-    print("Merge Sort")
-    print(merge_sort(to_be_sorted))
-    print("Quick Sort")
-    print(quick_sort(to_be_sorted))
+    end_time = time.time()
+    print(f"Runtime: {end_time - start_time:.6f} seconds\n")
 
+    print("Selection Sort")
+    start_time = time.time()
+    print(selection_sort(to_be_sorted))
+    end_time = time.time()
+    print(f"Runtime: {end_time - start_time:.6f} seconds\n")
+
+    print("Insertion Sort")
+    start_time = time.time()
+    print(insertion_sort(to_be_sorted))
+    end_time = time.time()
+    print(f"Runtime: {end_time - start_time:.6f} seconds\n")
+
+    print("Merge Sort")
+    start_time = time.time()
+    print(merge_sort(to_be_sorted))
+    end_time = time.time()
+    print(f"Runtime: {end_time - start_time:.6f} seconds\n")
+
+    print("Quick Sort")
+    start_time = time.time()
+    print(quick_sort(to_be_sorted))
+    end_time = time.time()
+    print(f"Runtime: {end_time - start_time:.6f} seconds\n")
+    
 if __name__ == "__main__":
     main()
